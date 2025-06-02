79a35ed0e67210d179e97599775256da4bfe7158
YourUsername <youremail@example.com>
2025-06-02 12:58:09 +0800

    scan_success

--- launch/ms200_scan_view.launch.py
+++ launch/ms200_scan_view.launch.py
@@ -7,33 +7,33 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
-  # RViZ2 settings
-  rviz2_config = os.path.join(
-      get_package_share_directory('oradar_lidar'),
-      'rviz2',
-      'oradar_scan.rviz'
-  )
-  
-  rviz2_node = Node(
-      package='rviz2',
-      node_executable='rviz2',
-      node_name='rviz2_show',
-      arguments=['-d',rviz2_config],
-      output='screen'
-  )
+    # RViZ2 settings
+    rviz2_config = os.path.join(
+        get_package_share_directory('oradar_lidar'),
+        'rviz2',
+        'oradar_scan.rviz'
+    )
+    
+    rviz2_node = Node(
+        package='rviz2',
+        executable='rviz2',         # 修改：node_executable -> executable
+        name='rviz2_show',          # 修改：node_name -> name
+        arguments=['-d', rviz2_config],
+        output='screen'
+    )
 
-  #Include OrdLidar launch file
-  ordlidar_launch = IncludeLaunchDescription(
-      launch_description_source=PythonLaunchDescriptionSource([
-          get_package_share_directory('oradar_lidar'),
-          '/launch/ms200_scan.launch.py'
-      ])
-  )
+    # Include OrdLidar launch file
+    ordlidar_launch = IncludeLaunchDescription(
+        launch_description_source=PythonLaunchDescriptionSource([
+            get_package_share_directory('oradar_lidar'),
+            '/launch/ms200_scan.launch.py'
+        ])
+    )
 
-  # Define LaunchDescription variable
-  ord = LaunchDescription()
+    # Define LaunchDescription variable
+    ord = LaunchDescription()
 
-  ord.add_action(ordlidar_launch)
-  ord.add_action(rviz2_node)
-  
-  return ord
+    ord.add_action(ordlidar_launch)
+    ord.add_action(rviz2_node)
+    
+    return ord
